581ff1591fd69c2faa3dd2b7ede0f51016f5cbee
Luiz Fernando <luizepauloxd@gmail.com>
2020-02-26 15:00:11 -0300

    ⚡️ Adds jest config

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'tsconfig.json',
+      babelConfig: true,
+    },
+  },
+  testMatch: ['**/__tests__/*.+(ts|tsx|js)'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+};
