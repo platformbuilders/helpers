b6e1aa4e71c7bfcde0ab262ecbfc48b0ff825cf5
Alexsandro Silva Alves <alexsandro-sa@MacBook-Pro-de-Alexsandro.local>
2020-07-13 18:56:25 -0300

    feat(test): add more test scenarios

--- src/__tests__/switchStyle.spec.ts
+++ src/__tests__/switchStyle.spec.ts
@@ -1,6 +1,12 @@
 import { switchStyle } from '../switchStyle';
 
 describe('Switch Style tests', () => {
+  it('should switch style with invalid', () => {
+    const value = switchStyle('no_exist')({ foo: 'test' })({ name: 'foo' });
+
+    expect(value).toBe(undefined);
+  });
+
   it('should switch style', () => {
     const value = switchStyle('name')({ foo: 'test' })({ name: 'foo' });
 
--- src/__tests__/toOnlyNumbers.spec.ts
+++ src/__tests__/toOnlyNumbers.spec.ts
@@ -1,6 +1,31 @@
 import { toOnlyNumbers } from '../toOnlyNumbers';
 
 describe('toOnlyNumbers tests', () => {
+  it('should format to string empty properly', () => {
+    const parsed = toOnlyNumbers('');
+    expect(parsed).toBe('');
+  });
+
+  it('should format to only space empty properly', () => {
+    const parsed = toOnlyNumbers(' ');
+    expect(parsed).toBe('');
+  });
+
+  it('should format to only string properly', () => {
+    const parsed = toOnlyNumbers('ABC');
+    expect(parsed).toBe('');
+  });
+
+  it('should format to some strings and numbers properly', () => {
+    const parsed = toOnlyNumbers('A1B2C3');
+    expect(parsed).toBe('123');
+  });
+
+  it('should format to some strings, numbers and special characters properly', () => {
+    const parsed = toOnlyNumbers('@A1!B2,C3-');
+    expect(parsed).toBe('123');
+  });
+
   it('should format to only numbers properly', () => {
     const parsed = toOnlyNumbers('R1000');
     expect(parsed).toBe('1000');
