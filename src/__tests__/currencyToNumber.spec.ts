83a8914acb2446ce28d89b8f4b7d07549fa5ac06
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-12-28 13:54:51 -0300

    feat: created tests to currency to number

--- src/__tests__/currencyToNumber.spec.ts
+++ src/__tests__/currencyToNumber.spec.ts
@@ -9,4 +9,12 @@ describe('currencyToNumber tests', () => {
     const parsed = currencyToNumber('R$ 1000,10');
     expect(parsed).toBe(1000.1);
   });
+  it('must analyze for millions', () => {
+    const parsed = currencyToNumber('R$ 1.000.000,00');
+    expect(parsed).toBe(1000000);
+  });
+  it('must analyze for millions with decimals', () => {
+    const parsed = currencyToNumber('R$ 1.000.000,50');
+    expect(parsed).toBe(1000000.5);
+  });
 });
