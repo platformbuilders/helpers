a35f3fa70cbc40a1045987afbae8a7ed0a04734d
fabinppkbuilders <84453168+fabinppkbuilders@users.noreply.github.com>
2021-08-24 09:22:14 -0300

    chore: uglify dist files (#365)
    Co-authored-by: Fabiano <fabinppk@hotmail.com>

--- src/switchStyle.ts
+++ src/switchStyle.ts
@@ -1,12 +1,13 @@
 import { isFunction } from 'lodash';
 
-export const switchStyle = (name: string) => (stylesObj: {
-  [x: string]: any;
-}) => (props: { [x: string]: any }): string => {
-  const propValue = props[name] || 'default';
-  const switchObj = stylesObj[propValue];
+export const switchStyle =
+  (name: string) =>
+  (stylesObj: { [x: string]: any }) =>
+  (props: { [x: string]: any }): string => {
+    const propValue = props[name] || 'default';
+    const switchObj = stylesObj[propValue];
 
-  if (isFunction(switchObj)) return switchObj(props);
+    if (isFunction(switchObj)) return switchObj(props);
 
-  return switchObj;
-};
+    return switchObj;
+  };
