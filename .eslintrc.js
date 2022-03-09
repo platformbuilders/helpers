2d00266d124338ae63a357dbb97a0f61ddbd1ec7
Lucas Diniz <md.lucasd@gmail.com>
2022-03-09 11:58:56 -0300

    update rollup and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: [
     'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
